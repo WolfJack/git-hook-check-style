6c77637862472357a44731600b7327c8408e855b
yaoyakun <yaoyakun@wondersgroup.com>
2019-10-30 15:39:54 +0800

    #init

---
+++ app/src/main/java/com/yyk/githook/githook/checkstyle/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.yyk.githook.githook.checkstyle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
