f283dd0f835057098efab0c3916225d340f37101
yaoyakun <yaoyakun@wondersgroup.com>
2019-10-31 10:55:54 +0800

    #init

--- app/src/main/java/com/yyk/githook/githook/checkstyle/TestActivity.java
+++ app/src/main/java/com/yyk/githook/githook/checkstyle/TestActivity.java
@@ -1,6 +1,19 @@
 package com.yyk.githook.githook.checkstyle;
 
 
-public class TestActivity {
+import android.os.Bundle;
+import android.os.PersistableBundle;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TestActivity extends AppCompatActivity {
+
+    private String aaa_bbb="dafda";
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        aaa_bbb="13223";
+    }
 }
